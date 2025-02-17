411891fe644e131ce1a8ea33ca2aba984e73fcf7
qminhhp <qminhhp@gmail.com>
2025-02-17 03:47:35 +0000

    Fix CF

--- src/App.tsx
+++ src/App.tsx
@@ -96,7 +96,7 @@ function App() {
                   </PrivateRoute>
                 }
               />
-              <Route path="/" element={<PageView />} />
+              <Route path="/" element={<LandingPage />} />
               <Route
                 path="/home"
                 element={
