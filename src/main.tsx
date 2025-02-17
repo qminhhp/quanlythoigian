c2c5d75547971c3a7536d09a6193c1e3ebe37e0e
qminhhp <qminhhp@gmail.com>
2025-02-17 09:37:52 +0000

    hihi sửa thôi bạn

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 // Error boundary for the entire app
 class ErrorBoundary extends React.Component<
