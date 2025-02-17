4810b34cdd57957e97c8f8bb371eef5a427bd873
qminhhp <qminhhp@gmail.com>
2025-02-17 09:30:51 +0000

    fix lỗi trắng trang

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Suspense, lazy } from "react";
+import * as React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./components/auth/AuthProvider";
 import { LanguageProvider } from "./lib/i18n/LanguageContext";
